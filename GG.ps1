f53bc3f43db1d74924a26a4b320c2d07efc8786f
mrizwanul-haq <116154287+mrizwanul-haq@users.noreply.github.com>
2024-02-27 12:55:40 +0100

    Update GG.ps1
    Added Register

--- GG.ps1
+++ GG.ps1
@@ -112,6 +112,21 @@ exit
 '@
 $SetCommand | Out-File -FilePath "C:\Windows\Autopilot.cmd" -Encoding ascii -Force
 
+$SetCommand = @'
+@echo off
+$AutopilotParams = @{
+    Online = $true
+    TenantId = '06bb15dc-3bc6-406d-b397-4f7fdd149de1'
+    AppId = '94c9ae95-81f8-45fd-976b-1de5e9aa8b08'
+    AppSecret = 'gpH8Q~GqCnjs0BZQj-jNPDFsN8FxDTNwcAeZzbH7'
+    GroupTag = 'GG'
+    Assign = $true
+}
+Get-WindowsAutoPilotInfo @AutopilotParams
+
+'@
+$SetCommand | Out-File -FilePath "C:\Windows\register.cmd" -Encoding ascii -Force
+
 
 #Restart from WinPE
 
