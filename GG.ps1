a9ecf56e9eadaf3f34fb16c552231b8477c9d956
mrizwanul-haq <116154287+mrizwanul-haq@users.noreply.github.com>
2024-04-02 13:31:50 +0200

    Update GG.ps1

--- GG.ps1
+++ GG.ps1
@@ -115,12 +115,12 @@ $SetCommand | Out-File -FilePath "C:\Windows\Autopilot.cmd" -Encoding ascii -For
 $SetCommand = @'
 @echo off
 $AutopilotParams = @{
-    Online = $true
-    TenantId = '06bb15dc-3bc6-406d-b397-4f7fdd149de1'
-    AppId = '94c9ae95-81f8-45fd-976b-1de5e9aa8b08'
-    AppSecret = 'gpH8Q~GqCnjs0BZQj-jNPDFsN8FxDTNwcAeZzbH7'
-    GroupTag = 'GG'
-    Assign = $true
+    "Online" = $true
+    "TenantId" = '06bb15dc-3bc6-406d-b397-4f7fdd149de1'
+    "AppId" = '94c9ae95-81f8-45fd-976b-1de5e9aa8b08'
+    "AppSecret" = 'gpH8Q~GqCnjs0BZQj-jNPDFsN8FxDTNwcAeZzbH7'
+    "GroupTag" = 'GG'
+    "Assign" = $true
 }
 start "Get-WindowsAutoPilotInfo" PowerShell -NoL -C Get-WindowsAutoPilotInfo @AutopilotParams
 
