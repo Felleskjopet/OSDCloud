caa3d0f4016792dc9ed9e12fb73938cee9c08ddb
mrizwanul-haq <116154287+mrizwanul-haq@users.noreply.github.com>
2024-04-02 13:59:56 +0200

    Update GG.ps1

--- GG.ps1
+++ GG.ps1
@@ -114,15 +114,7 @@ $SetCommand | Out-File -FilePath "C:\Windows\Autopilot.cmd" -Encoding ascii -For
 
 $SetCommand = @'
 @echo off
-$AutopilotParams = @{
-    "Online" = $true
-    "TenantId" = '06bb15dc-3bc6-406d-b397-4f7fdd149de1'
-    "AppId" = '94c9ae95-81f8-45fd-976b-1de5e9aa8b08'
-    "AppSecret" = 'gpH8Q~GqCnjs0BZQj-jNPDFsN8FxDTNwcAeZzbH7'
-    "GroupTag" = 'GG'
-    "Assign" = $true
-}
-start "Get-WindowsAutoPilotInfo" PowerShell -NoL -C Get-WindowsAutoPilotInfo @AutopilotParams
+Start /Wait PowerShell -NoL -C Invoke-WebPSScript https://raw.githubusercontent.com/Felleskjopet/OSDCloud/main/ap.ps1
 
 '@
 $SetCommand | Out-File -FilePath "C:\Windows\register.cmd" -Encoding ascii -Force
