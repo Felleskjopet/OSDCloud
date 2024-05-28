9ab2bfabebd6e8fd9c50106f1964d8c9e8357a38
mrizwanul-haq <116154287+mrizwanul-haq@users.noreply.github.com>
2024-05-28 08:12:07 +0200

    Update FK.ps1

--- FK.ps1
+++ FK.ps1
@@ -106,7 +106,7 @@ Start /Wait PowerShell -NoL -C Start-OOBEDeploy
 start PowerShell -NoL -W Mi
 reg delete HKEY_LOCAL_MACHINE\SOFTWARE\Microsoft\Provisioning\Diagnostics\AutoPilot /va /f
 start "Install-Module AutopilotOOBE" /wait PowerShell -NoL -C Install-Module AutopilotOOBE -Force -Verbose
-start "Start-AutopilotOOBE" PowerShell -NoL -C Start-AutopilotOOBE -Title 'FK Autopilot Registration' -Hidden AssignedUser, AssignedComputerName -GroupTag FK -GroupTagOptions FK, SA -AddToGroupOptions 'MEM - Flog Surface, INTUNE_FK_DEVICES_USB' -Assign -PostAction Restart Computer
+start "Start-AutopilotOOBE" PowerShell -NoL -C Start-AutopilotOOBE -Title 'FK Autopilot Registration' -Hidden AssignedUser, AssignedComputerName -GroupTag FK -GroupTagOptions FK, SA -AddToGroupOptions 'MEM - Flog Surface', 'INTUNE_FK_DEVICES_USB' -Assign -PostAction Restart Computer
 
 exit
 '@
