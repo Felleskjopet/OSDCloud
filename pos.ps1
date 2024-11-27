42a7c10bbca0acec162177a35681e56f5eefc6a0
mrizwanul-haq <116154287+mrizwanul-haq@users.noreply.github.com>
2024-11-27 09:11:45 +0100

    Update pos.ps1

--- pos.ps1
+++ pos.ps1
@@ -2,15 +2,15 @@
 [Console]::OutputEncoding = [System.Text.Encoding]::UTF8
 
 # Informere brukeren om konsekvensene
-Write-Host "⚠️ Varning: Denna process kommer att reinstallera systemet och radera ALLT innehåll på hårddisken!" -ForegroundColor Red
-Write-Host "Vill du fortsätta? Tryck 'J' för Ja eller 'N' för Nej." -ForegroundColor Yellow
+Write-Host "⚠️ Warning: This process will reinstall the system and erase ALL contents of the hard drive!" -ForegroundColor Red
+Write-Host "Do you want to continue? Press 'Y' for Yes or 'N' for No" -ForegroundColor Yellow
 
 # Spør brukeren om bekreftelse
-$svaret = Read-Host "Ditt val"
+$svaret = Read-Host "Your choice"
 
 # Kontrollere brukerens svar
-if ($svaret -eq 'J') {
-    Write-Host "Startar OSDCloud-processen... Systemet kommer att reinstallera och radera all data på hårddisken!" -ForegroundColor Green
+if ($svaret -eq 'Y') {
+    Write-Host "Startar OSDCloud-processen... The system will install and delete all data on the hard drive!" -ForegroundColor Green
     Start-OSDCloud -ImageFileUrl https://managementosdcloudst.blob.core.windows.net/osd/Win_LTSC_2021_SV.wim -ImageIndex 1 -ZTI
     Write-Host -ForegroundColor Green "Restarting in 10 seconds!"
 
